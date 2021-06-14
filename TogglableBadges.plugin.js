5d40854eb485fcd687a11510bc516dec5f90c50d
Gabe616 <60316309+Gabe616@users.noreply.github.com>
2021-06-14 06:26:54 +0000

    update or whatever

--- TogglableBadges.plugin.js
+++ TogglableBadges.plugin.js
@@ -34,143 +34,103 @@
 @else@*/
 
 const TogglableBadges = (() => {
-  const config = {
-    "main": "index.js",
-    "info": {
-      "name": "Togglable Badges",
-      "authors": [
-        {
-          "name": "pog fish",
-          "discord_id": "783404330573234216",
-          "github_username": "Gabe616"
-        }
-      ],
-      "version": "1.3.0",
-      "description": "Allows you to toggle certain badges",
-      "github": "https://github.com/Gabe616/TogglableBadges/blob/master/TogglableBadges.plugin.js",
-      "github_raw": "https://raw.githubusercontent.com/Gabe616/TogglableBadges/master/TogglableBadges.plugin.js",
-      "icon_folder": "https://raw.githubusercontent.com/Gabe616/TogglableBadges/master/icons/"
-    },
-    "changelog": [
-      {
-        "title": "Bug Fixes",
-        "type": "fixed",
-        "items": ["The plugin actually saves shit"]
+    const config = {
+      "main": "index.js",
+      "info": {
+        "name": "Togglable Badges",
+        "authors": [
+          {
+            "name": "pog fish",
+            "discord_id": "783404330573234216",
+            "github_username": "Gabe616"
+          }
+        ],
+        "version": "1.3.0",
+        "description": "Allows you to toggle certain badges",
+        "github": "https://github.com/Gabe616/TogglableBadges/blob/master/TogglableBadges.plugin.js",
+        "github_raw": "https://raw.githubusercontent.com/Gabe616/TogglableBadges/master/TogglableBadges.plugin.js",
+        "icon_folder": "https://raw.githubusercontent.com/Gabe616/TogglableBadges/master/icons/"
       },
-      {
-        "title": "TODO",
-        "type": "improved",
-        "items": ["Nitro badges"]
-      }
-    ]
-  };
-
-  return !global.ZeresPluginLibrary ? class {
-        constructor() {this._config = config;}
-        getName() {return config.info.name;}
-        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
-        getDescription() {return config.info.description;}
-        getVersion() {return config.info.version;}
-        load() {
-            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
-                confirmText: "Download Now",
-                cancelText: "Cancel",
-                onConfirm: () => {
-                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
-                    });
-                }
-            });
+      "changelog": [
+        {
+          "title": "Bug Fixes",
+          "type": "fixed",
+          "items": ["The plugin actually saves shit"]
+        },
+        {
+          "title": "TODO",
+          "type": "improved",
+          "items": ["Nitro badges"]
         }
-        start() {}
-        stop() {}
-    } : (([Plugin, Api]) => {
-      const plugin = (Plugin, Library) => {
-        const {Logger, Patcher, Settings} = Library;
-
-        return class TogglableBadges extends Plugin {
-          constructor() {
-            super();
-            this.badges = {
-STAFF:                       { label: "Staff", value: 1 << 0 },
-DISCORD_PARTNER:             { label: "Discord Partner", value: 1 << 1 },
-HYPESQUAD_EVENTS:            { label: "Hypesquad Events", value: 1 << 2 },
-BUGHUNTER_LVL_1:             { label: "Bughunter Level 1", value: 1 << 3 },
-HOUSE_BRAVERY:               { label: "Hypesquad House of Bravery", value: 1 << 6 },
-HOUSE_BRILLIANCE:            { label: "Hypesquad House of Brilliance", value: 1 << 7 },
-HOUSE_BALANCE:               { label: "Hypesquad House of Balance", value: 1 << 8 },
-EARLY_SUPPORTER:             { label: "Early Supporter", value: 1 << 9 },
-BUGHUNTER_LVL_2:             { label: "Bughunter Level 2", value: 1 << 14 },
-EARLY_VERIFIED_DEVELOPER:    { label: "Early Verified Developer", value: 1 << 17 },
-DISCORD_CERTIFIED_MODERATOR: { label: "Discord Certified Moderator", value: 1 << 18 }
-            }
-            let lol = {};
-            for (let i = 0; i < Object.keys(this.badges).length; i++) {
-              let k = Object.keys(this.badges)[i];
-              lol[k.toLowerCase()] = false;
-            }
-            this.defaultSettings = {
-              badges: lol
-            }
+      ]
+    };
+  
+    return !global.ZeresPluginLibrary ? class {
+          constructor() {this._config = config;}
+          getName() {return config.info.name;}
+          getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+          getDescription() {return config.info.description;}
+          getVersion() {return config.info.version;}
+          load() {
+              BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+                  confirmText: "Download Now",
+                  cancelText: "Cancel",
+                  onConfirm: () => {
+                      require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                          if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                          await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                      });
+                  }
+              });
           }
-
-          onStart() {
-            this.updateFlags();
-          }
-
-          onStop() {
-            this.updateFlags();
-          }
-
-          updateFlags() {
-            let webpack = Object.values(window.webpackJsonp.push([ [], { [''] : (_, e, r) => { e.cache = r.c }}, [ [''] ] ] ).cache);
-            let user = webpack.find(m => m.exports && m.exports.default && !!m.exports.default.getCurrentUser).exports.default.getCurrentUser();
-            user.flags = Object.keys(this.settings.badges).filter(k => !!this.settings.badges[k]).map(a => this.badges[a.toUpperCase()].value).reduce((a, b) => a | b, 0);
-          }
-
-          getSettingsPanel(collapseStates = {}) {
-            /*const panel = this.buildSettingsPanel();
-            panel.append(this.buildSetting({
-              title: "Badges:",
-              children: Object.keys(this.badges).map(key => ({
-                type: "Switch",
-                plugin: this,
-                keys: ["badges", key],
-                label: this.badges[key].label,
-                value: this.settings.badges[key],
-                labelChildren: [
-									BDFDB.ReactUtils.createElement("img", {style: {width: 28, height: 28}, src: config.info.icon_folder + key})
-								]
-              }))
-            }));
-            return panel.getElement();
-
-            return BDFDB.PluginUtils.createSettingsPanel(this, {
-              collapseStates,
-              children: () => {
-                let panel = [];
-
-                panel.push(BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.SettingsPanelList, {
-                  title: "Badges:",
-                  children: Object.keys(this.badges).map(key => BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.SettingsSaveItem, {
-                    type: "Switch",
-                    plugin: this,
-                    keys: ["badges", key],
-                    label: this.badges[key].label,
-                    value: this.settings.badges[key],
-                    labelChildren: [
-                      BDFDB.ReactUtils.createElement("img", {style: {width: 28, height: 28}, src: config.info.icon_folder + key})
-                    ]
-                  }))
-                }));
-
-                return panel;
+          start() {}
+          stop() {}
+      } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Library) => {
+          const {Logger, Patcher, Settings} = Library;
+  
+          return class TogglableBadges extends Plugin {
+            constructor() {
+              super();
+              this.badges = {
+  STAFF:                       { label: "Staff", value: 1 << 0 },
+  DISCORD_PARTNER:             { label: "Discord Partner", value: 1 << 1 },
+  HYPESQUAD_EVENTS:            { label: "Hypesquad Events", value: 1 << 2 },
+  BUGHUNTER_LVL_1:             { label: "Bughunter Level 1", value: 1 << 3 },
+  HOUSE_BRAVERY:               { label: "Hypesquad House of Bravery", value: 1 << 6 },
+  HOUSE_BRILLIANCE:            { label: "Hypesquad House of Brilliance", value: 1 << 7 },
+  HOUSE_BALANCE:               { label: "Hypesquad House of Balance", value: 1 << 8 },
+  EARLY_SUPPORTER:             { label: "Early Supporter", value: 1 << 9 },
+  BUGHUNTER_LVL_2:             { label: "Bughunter Level 2", value: 1 << 14 },
+  EARLY_VERIFIED_DEVELOPER:    { label: "Early Verified Developer", value: 1 << 17 },
+  DISCORD_CERTIFIED_MODERATOR: { label: "Discord Certified Moderator", value: 1 << 18 }
               }
-            });*/
-
-            
-            return Settings.SettingPanel.build(this.saveSettings.bind(this),
+              let lol = {};
+              for (let i = 0; i < Object.keys(this.badges).length; i++) {
+                let k = Object.keys(this.badges)[i];
+                lol[k.toLowerCase()] = false;
+              }
+              this.defaultSettings = {
+                badges: lol
+              }
+            }
+  
+            onStart() {
+              this.updateFlags();
+            }
+  
+            onStop() {
+              this.updateFlags();
+            }
+  
+            updateFlags() {
+              let webpack = Object.values(window.webpackJsonp.push([ [], { [''] : (_, e, r) => { e.cache = r.c }}, [ [''] ] ] ).cache);
+              let user = webpack.find(m => m.exports && m.exports.default && !!m.exports.default.getCurrentUser).exports.default.getCurrentUser();
+              user.flags = Object.keys(this.settings.badges).filter(k => !!this.settings.badges[k]).map(a => this.badges[a.toUpperCase()].value).reduce((a, b) => a | b, 0);
+            }
+  
+            getSettingsPanel(collapseStates = {}) {
+              return Settings.SettingPanel.build(this.saveSettings.bind(this),
               new Settings.SettingGroup("Badges").append(...Object.keys(this.badges).map(key => {
                 let a = this.badges[key];
                 return new Settings.Switch(a.label, "", this.settings.badges[key.toLowerCase()], (e) => {
@@ -179,11 +139,11 @@ DISCORD_CERTIFIED_MODERATOR: { label: "Discord Certified Moderator", value: 1 <<
                 });
               }))
             );
+            }
           }
         }
-      }
-      return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
+        return plugin(Plugin, Api);
+      })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
 
 /*@end@*/
